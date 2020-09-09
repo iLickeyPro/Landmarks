959a4141512b4dc006bc801394be49e0a722bc29
iLickeyPro <lickeymut@icloud.com>
2020-09-10 01:59:22 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 野口 勇気 on 2020/09/10.
+//  Copyright © 2020 Yuki Noguchi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
